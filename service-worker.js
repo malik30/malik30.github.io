065070d05c5ea6b5fea328986f8483df45a4ee54
malik30 <malik.trp@gmail.com>
2018-12-04 12:40:00 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+self.addEventListener("install", function(e){
+  console.log("install");
+});
+
+self.addEventListener("activate", function(e){
+  console.log("activate");
+});
+
+self.addEventListener("fetch", function(e){
+  console.log("fetch");
+  event.respondWith(
+      "a";
+  );
+});
